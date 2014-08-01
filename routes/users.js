bd17c5373e3425b9c415674683737fbd2226f22e
benpoor <saske1984@126.com>
2014-08-01 17:59:38 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var mysql = require("mysql");
 var router = express.Router();
 
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'123456',
+    database:'huajiemu'
+});
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  var name = "";
+//  connection.connect();
+    connection.query('select * from users', function(err,rows,fields){
+        if(err){
+            throw err;
+        }
+        name = rows[0].name
+        res.send('respond with a resource '+name);
+    });
+//    connection.end();
+
 });
 
 module.exports = router;
